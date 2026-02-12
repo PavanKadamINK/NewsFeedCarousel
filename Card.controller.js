d0390bd6fd748b1599bda9475df0049189d799ab
Pavan K <pavan.k@inkitsolutions.com.au>
2026-02-12 12:30:46 +0000

    remove debugger

--- Card.controller.js
+++ Card.controller.js
@@ -12,18 +12,12 @@ sap.ui.define([
 			var cardId = "com.winslow.yve.newsfeed";
 			cardId = cardId.replace(/\./g, '/');
 			var oImgModel = new JSONModel({
-				// Image_1 : sap.ui.require.toUrl(cardId + "/images") + "/Newfeed3.png",
-				// Image_2 : sap.ui.require.toUrl(cardId + "/images") + "/Newfeed2.png",
-				// Image_3 : sap.ui.require.toUrl(cardId + "/images") + "/Newsfeed1.png",
-				// Image_4 : sap.ui.require.toUrl(cardId + "/images") + "/Newfeed4.png",
-				// Image_5 : sap.ui.require.toUrl(cardId + "/images") + "/Newfeed5.png",
-
 				Image_1: sap.ui.require.toUrl(cardId + "/images") + "/community_updates.jpg",
 				Image_2: sap.ui.require.toUrl(cardId + "/images") + "/project_updates.jpg",
 				Image_3: sap.ui.require.toUrl(cardId + "/images") + "/tream_updates.jpg",
 			});
 			this.getView().setModel(oImgModel, "images");
-			debugger
+			// debugger
 			var oCarousel = this.byId("carouselSample");
 			oCarousel.destroyPages();
 			var x = Object.keys(oImgModel.getData());
